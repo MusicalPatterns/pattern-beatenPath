8e1e107174a0067ac0f4c3563f6be1a621dbcaa2
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-10-02 14:43:10 -0700

    finish up test

--- spec/durations.test.ts
+++ spec/durations.test.ts
@@ -1,53 +1,78 @@
 import { beatenPathDurations } from '../src/durations'
+import isCloseTo from '../../../spec/support/isCloseTo'
 
 describe('beaten path durations', () => {
     it('first duration is 1', () => {
-        expect(beatenPathDurations[0]).toBe(1)
+        expect(isCloseTo(beatenPathDurations[0], 1)).toBeTruthy()
     })
 
     it('block 1 - 5/6 of previous block', () => {
-        expect(beatenPathDurations[1]).toBe(beatenPathDurations[0] * 5 / 6)
+        expect(isCloseTo(beatenPathDurations[1], beatenPathDurations[0] * 5 / 6)).toBeTruthy()
     })
-
     it('block 2 - 5/4 of previous block', () => {
-        expect(beatenPathDurations[2]).toBe(beatenPathDurations[1] * 5 / 4)
+        expect(isCloseTo(beatenPathDurations[2], beatenPathDurations[1] * 5 / 4)).toBeTruthy()
     })
 
     it('block 3 - 5/6 of previous block', () => {
-        expect(beatenPathDurations[3]).toBe(beatenPathDurations[2] * 5 / 6)
+        expect(isCloseTo(beatenPathDurations[3], beatenPathDurations[2] * 5 / 6)).toBeTruthy()
     })
-
     it('block 4 - 5/4 of previous block', () => {
-        expect(beatenPathDurations[4]).toBe(beatenPathDurations[3] * 5 / 4)
+        expect(isCloseTo(beatenPathDurations[4], beatenPathDurations[3] * 5 / 4)).toBeTruthy()
     })
 
     it('block 5 - 5/6 of previous block', () => {
-        expect(beatenPathDurations[5]).toBe(beatenPathDurations[4] * 5 / 6)
+        expect(isCloseTo(beatenPathDurations[5], beatenPathDurations[4] * 5 / 6)).toBeTruthy()
+    })
+    it('block 6 - 5/4 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[6], beatenPathDurations[5] * 5 / 4)).toBeTruthy()
     })
 
-    it('block 5 - 5/6 of previous block', () => {
-        expect(beatenPathDurations[6]).toBe(beatenPathDurations[5] * 5 / 4)
+    it('block 7 - 5/6 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[7], beatenPathDurations[6] * 5 / 6)).toBeTruthy()
+    })
+    it('block 8 - 5/4 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[8], beatenPathDurations[7] * 5 / 4)).toBeTruthy()
     })
 
-    // 5/4
+    it('block 9 - 5/6 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[9], beatenPathDurations[8] * 5 / 6)).toBeTruthy()
+    })
+    it('block 10 - ALSO 5/6 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[10], beatenPathDurations[9] * 5 / 6)).toBeTruthy()
+    })
+    it('block 11 - 5/4 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[11], beatenPathDurations[10] * 5 / 4)).toBeTruthy()
+    })
 
-    // 5/6
-    // 5/4
+    it('block 12 - 5/6 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[12], beatenPathDurations[11] * 5 / 6)).toBeTruthy()
+    })
+    it('block 13 - 5/4 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[13], beatenPathDurations[12] * 5 / 4)).toBeTruthy()
+    })
 
-    // 5/6
-    // 5/6
-    // 5/4
+    it('block 14 - 5/6 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[14], beatenPathDurations[13] * 5 / 6)).toBeTruthy()
+    })
+    it('block 15 - 5/4 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[15], beatenPathDurations[14] * 5 / 4)).toBeTruthy()
+    })
 
-    // 5/6
-    // 5/4
+    it('block 16 - 5/6 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[16], beatenPathDurations[15] * 5 / 6)).toBeTruthy()
+    })
+    it('block 17 - 5/4 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[17], beatenPathDurations[16] * 5 / 4)).toBeTruthy()
+    })
 
-    // 5/6
-    // 5/4
+    it('block 18 - 5/6 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[18], beatenPathDurations[17] * 5 / 6)).toBeTruthy()
+    })
+    it('block 19 - 5/4 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[19], beatenPathDurations[18] * 5 / 4)).toBeTruthy()
+    })
 
-    // 5/6
-    // 5/4
-
-    // 5/6
-    // 5/6
-    // 5/4
+    it('block 0 (WRAPPING AROUND) - 5/6 of previous block', () => {
+        expect(isCloseTo(beatenPathDurations[0], beatenPathDurations[19] * 5 / 6)).toBeTruthy()
+    })
 })
--- src/durations.ts
+++ src/durations.ts
@@ -3,7 +3,7 @@ import { beatenPathRatios } from './ratios'
 const beatenPathDurations: number[] = [1]
 
 // tslint:disable-next-line:prefer-for-of
-for (let i: number = 0; i < beatenPathRatios.length; i++) {
+for (let i: number = 0; i < beatenPathRatios.length - 1; i++) {
     const ratio: number = (beatenPathRatios[i][0] / beatenPathRatios[i][1])
     const previousDuration: number = beatenPathDurations[beatenPathDurations.length - 1]
     beatenPathDurations.push(previousDuration * ratio)
