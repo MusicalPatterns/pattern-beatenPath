c2acd1068c637e2e11d2ca52ba34a81cf9feab03
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-10-27 14:44:12 -0700

    no building stuff at top-level module scope; defer to functions for building things when needed

--- src/scales.ts
+++ src/scales.ts
@@ -1,17 +1,21 @@
-import { BuildScalesFunction, flatDurationsScale, Scales, SongSpec } from '../../../src'
+import { BuildScalesFunction, buildStandardScales, Scale, SongSpec } from '../../../src'
 
-const buildBeatenPathScales: BuildScalesFunction = (songSpec: SongSpec): Scales =>
-    [
-        flatDurationsScale,
-        {
-            scalar: songSpec.songDurationScalar,
-            scalars: flatDurationsScale.scalars,
-        },
-        {
-            scalar: songSpec.songPitchScalar,
-            scalars: flatDurationsScale.scalars,
-        },
-    ]
+const buildBeatenPathScales: BuildScalesFunction =
+    (songSpec: SongSpec): Scale[] => {
+        const { flatDurationsScale } = buildStandardScales()
+
+        return [
+            flatDurationsScale,
+            {
+                scalar: songSpec.songDurationScalar,
+                scalars: flatDurationsScale.scalars,
+            },
+            {
+                scalar: songSpec.songPitchScalar,
+                scalars: flatDurationsScale.scalars,
+            },
+        ]
+    }
 
 export {
     buildBeatenPathScales,
