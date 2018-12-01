e1b158c0a88c0a87c5808bdc3361ced4bea46cf1
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-30 22:12:54 -0800

    now all patterns export not just their snapshot but also their configuration so they can be customized if you also import the compiler

---
+++ package/index.d.ts
@@ -0,0 +1,8 @@
+// tslint:disable:no-implicit-dependencies
+
+import { Pattern } from '@musical-patterns/pattern';
+import { ThreadSpec } from '@musical-patterns/performer';
+declare const snapshot: ThreadSpec[];
+declare const beatenPathPattern: Pattern;
+export { beatenPathPattern };
+export default snapshot;
