9935db059ea97f9ac90d63825f1be3e1262ad1d8
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-27 12:33:06 -0800

    compiler now manages defaulting sustain to close to duration

--- src/constants.ts
+++ src/constants.ts
@@ -3,14 +3,12 @@
 import { Count, Scalar, to } from '@musical-patterns/utilities'
 import { Core, to as beatenPathTo } from './nominal'
 
-const SUSTAIN_AMOUNT: Scalar = to.Scalar(0.9)
 const MINIMUM_FUNCTIONAL_CORE: Core = beatenPathTo.Core(2)
 const BEATEN_PATH_INITIAL_CORE: Core = beatenPathTo.Core(5)
 const BEATEN_PATH_INITIAL_REPETITIONS: Count = to.Count(1)
 const BEATEN_PATH_PITCH_SCALAR: Scalar = to.Scalar(220)
 
 export {
-    SUSTAIN_AMOUNT,
     MINIMUM_FUNCTIONAL_CORE,
     BEATEN_PATH_INITIAL_CORE,
     BEATEN_PATH_INITIAL_REPETITIONS,
--- src/materials/notes.ts
+++ src/materials/notes.ts
@@ -1,7 +1,6 @@
 import { NoteSpec } from '@musical-patterns/compiler'
 import { DEFAULT_DURATIONS_SCALE_INDEX, DEFAULT_PITCH_SCALE_INDEX } from '@musical-patterns/pattern'
-import { apply, from, Scalar, to } from '@musical-patterns/utilities'
-import { SUSTAIN_AMOUNT } from '../constants'
+import { from, Scalar, to } from '@musical-patterns/utilities'
 
 const buildNoteSpec: (durationScalar: Scalar) => NoteSpec =
     (durationScalar: Scalar): NoteSpec => ({
@@ -12,10 +11,6 @@ const buildNoteSpec: (durationScalar: Scalar) => NoteSpec =
         pitchSpec: {
             scalar: to.Scalar(1 / from.Scalar(durationScalar)),
             scaleIndex: DEFAULT_PITCH_SCALE_INDEX,
-        },
-        sustainSpec: {
-            scalar: apply.Scalar(durationScalar, SUSTAIN_AMOUNT),
-            scaleIndex: DEFAULT_DURATIONS_SCALE_INDEX,
         },
     })
 
