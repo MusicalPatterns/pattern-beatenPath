94626fbfd98bf21df121fd3254640c40b4f96496
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-03 16:15:49 -0800

    sharing config is now managed by utilities-pattern

--- test/snapshot.test.ts
+++ test/snapshot.test.ts
@@ -6,6 +6,12 @@ describe('snapshot', () => {
         // tslint:disable-next-line:no-unsafe-any no-require-imports
         const { pattern, snapshot } = require('../src/indexForTest')
 
+        if (!pattern) {
+            done()
+
+            return
+        }
+
         // tslint:disable-next-line:no-unsafe-any
         expect(JSON.stringify(await compilePattern(pattern), undefined, 2))
             .toEqual(JSON.stringify(snapshot, undefined, 2))
