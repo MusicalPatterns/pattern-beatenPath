94eaf3ecc0f4af02e31d6ad29fbb551ed4a8cd5e
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-10 15:58:28 -0800

    travis experiment with ts-loader

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader',
+                loader: 'ts-loader',
                 exclude: /test/,
             },
             {
