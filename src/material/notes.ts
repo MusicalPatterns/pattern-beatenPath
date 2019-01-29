5999885123d0d64dac6e0717bfafb8c489954653
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-29 12:46:41 -0800

    1.0.114: standards not defaults

--- src/material/notes.ts
+++ src/material/notes.ts
@@ -1,16 +1,16 @@
 import { NoteSpec } from '@musical-patterns/compiler'
-import { DEFAULT_DURATIONS_SCALE_INDEX, DEFAULT_PITCH_SCALE_INDEX } from '@musical-patterns/pattern'
+import { STANDARD_DURATIONS_SCALE_INDEX, STANDARD_PITCH_SCALE_INDEX } from '@musical-patterns/pattern'
 import { from, Scalar, to } from '@musical-patterns/utilities'
 
 const buildNoteSpec: (durationScalar: Scalar) => NoteSpec =
     (durationScalar: Scalar): NoteSpec => ({
         durationSpec: {
             scalar: durationScalar,
-            scaleIndex: DEFAULT_DURATIONS_SCALE_INDEX,
+            scaleIndex: STANDARD_DURATIONS_SCALE_INDEX,
         },
         pitchSpec: {
             scalar: to.Scalar(1 / from.Scalar(durationScalar)),
-            scaleIndex: DEFAULT_PITCH_SCALE_INDEX,
+            scaleIndex: STANDARD_PITCH_SCALE_INDEX,
         },
     })
 
