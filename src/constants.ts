237526ea775a2f4ad2623c4994f14df70f221825
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-05 10:32:43 -0800

    1.0.119: scientific pitch

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,20 @@
 // tslint:disable no-magic-numbers
 
-import { Count, Frequency, Scalar, to } from '@musical-patterns/utilities'
+import {
+    Count,
+    Frequency,
+    SCIENTIFIC_PITCHES,
+    ScientificPitchNoteName,
+    ScientificPitchOctaveNumber,
+    to,
+} from '@musical-patterns/utilities'
 import { Core, to as beatenPathTo } from './nominal'
 
 const MINIMUM_FUNCTIONAL_CORE: Core = beatenPathTo.Core(2)
 const BEATEN_PATH_INITIAL_CORE: Core = beatenPathTo.Core(5)
 const BEATEN_PATH_INITIAL_REPETITIONS: Count = to.Count(1)
-const BEATEN_PATH_INITIAL_BASE_FREQUENCY: Frequency = to.Frequency(220)
+const BEATEN_PATH_INITIAL_BASE_FREQUENCY: Frequency =
+    SCIENTIFIC_PITCHES[ ScientificPitchNoteName.A ][ ScientificPitchOctaveNumber._3 ]
 const BEATEN_PATH_INITIAL_REVERSE: boolean = false
 
 export {
