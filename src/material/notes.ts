d3823988aee42b84d45690b377452614c853b640
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-04 13:11:57 -0800

    1.0.153: standardize build note spec

--- src/material/notes.ts
+++ src/material/notes.ts
@@ -1,32 +1,23 @@
 import { NoteSpec } from '@musical-patterns/compiler'
-import {
-    PitchDuration,
-    STANDARD_DURATIONS_SCALE_INDEX,
-    STANDARD_PITCH_SCALE_INDEX,
-    unpackPitchDurationContourElement,
-} from '@musical-patterns/pattern'
+import { PitchDuration, STANDARD_DURATIONS_SCALE_INDEX, STANDARD_PITCH_SCALE_INDEX } from '@musical-patterns/pattern'
 import { apply, ContourElement, to } from '@musical-patterns/utilities'
 import { SUSTAIN_AMOUNT } from './constants'
 
 const buildNoteSpec: (pitchDurationContourElement: ContourElement<PitchDuration>) => NoteSpec =
-    (contourElement: ContourElement<PitchDuration>): NoteSpec => {
-        const { pitch, duration } = unpackPitchDurationContourElement(contourElement)
-
-        return {
-            durationSpec: {
-                scalar: to.Scalar(duration),
-                scaleIndex: STANDARD_DURATIONS_SCALE_INDEX,
-            },
-            pitchSpec: {
-                scalar: to.Scalar(pitch),
-                scaleIndex: STANDARD_PITCH_SCALE_INDEX,
-            },
-            sustainSpec: {
-                scalar: apply.Scalar(to.Scalar(duration), SUSTAIN_AMOUNT),
-                scaleIndex: STANDARD_DURATIONS_SCALE_INDEX,
-            },
-        }
-    }
+    ([ pitch, duration ]: ContourElement<PitchDuration>): NoteSpec => ({
+        durationSpec: {
+            scalar: to.Scalar(duration),
+            scaleIndex: STANDARD_DURATIONS_SCALE_INDEX,
+        },
+        pitchSpec: {
+            scalar: to.Scalar(pitch),
+            scaleIndex: STANDARD_PITCH_SCALE_INDEX,
+        },
+        sustainSpec: {
+            scalar: apply.Scalar(to.Scalar(duration), SUSTAIN_AMOUNT),
+            scaleIndex: STANDARD_DURATIONS_SCALE_INDEX,
+        },
+    })
 
 export {
     buildNoteSpec,
