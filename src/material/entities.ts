861a8177bf964671e652d477273e9148d8ded816
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-15 22:33:27 -0700

    1.0.176: use TimbreName from compiler now so no ts-ignore

--- src/material/entities.ts
+++ src/material/entities.ts
@@ -1,4 +1,4 @@
-import { Entity, MaterializeEntities, Note, TimbreNameEnum } from '@musical-patterns/compiler'
+import { Entity, MaterializeEntities, Note, TimbreName, TimbreNameEnum } from '@musical-patterns/compiler'
 import { apply, Cycle, map, Ordinal, to } from '@musical-patterns/utilities'
 import { BeatenPathSpecs } from '../spec'
 import { computeEntitiesNotes } from './custom'
@@ -7,7 +7,7 @@ const materializeEntities: MaterializeEntities =
     (specs: BeatenPathSpecs): Entity[] => {
         const entitiesNotes: Note[][] = computeEntitiesNotes(specs)
 
-        const timbreNames: Cycle<string> = to.Cycle([
+        const timbreNames: Cycle<TimbreName> = to.Cycle([
             TimbreNameEnum.PUTNEY_WAVERING,
             TimbreNameEnum.ORGAN_2,
             TimbreNameEnum.PHONEME_OOH,
@@ -18,7 +18,6 @@ const materializeEntities: MaterializeEntities =
             entitiesNotes,
             (entityNotes: Note[], timbreNameIndex: Ordinal): Entity => ({
                 notes: entityNotes,
-                // @ts-ignore
                 timbreName: apply.Ordinal(timbreNames, timbreNameIndex),
             }),
         )
